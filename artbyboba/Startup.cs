ffe68c7ac34d542594a588de13dd1fa903c894c6
Corey Boba <coreyboba@gmail.com>
2015-09-29 23:52:00 -0400

    Initial commit.

---
+++ artbyboba/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(artbyboba.Startup))]
+namespace artbyboba
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
